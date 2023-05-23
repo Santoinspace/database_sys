1be97ad751830f1e611a865b8acca9c29f2714c7
Santo <1837904813@qq.com>
2023-05-24 00:33:32 +0800

    第一次修改

--- PHP/datafinal.php
+++ PHP/datafinal.php
@@ -6,6 +6,7 @@
         <link rel="stylesheet" href="../CSS/data.css">
     </head>
     <body>
+
         <form action='' method='post'>
         <div id="table">
             <h3 id='head'>请输入sql语句：</h3>
@@ -14,9 +15,12 @@
         </div>
         </form>
         <?php 
+            #echo "<script type='text/javascript'>fflush();</script>";
+            #连接xmapp自带的数据库
             $link = mysqli_connect('localhost','root','','test',3306) or die("connection failed");
             mysqli_set_charset($link,'utf8');
             echo "<br><br>";
+            #首次输出所有的列名称（默认表的）
             $col = mysqli_query($link,"SHOW COLUMNS FROM table1");
             echo "这是所有的属性：";
             while($colName = mysqli_fetch_assoc($col)){
@@ -24,7 +28,6 @@
                 echo " ";
             }
             echo "<br>";
-
             echo "<br>";
             #output all the row in the table "table1"
             $sql = mysqli_query($link,"select *from table1");
@@ -36,33 +39,35 @@
                 #$message[] = $row;
             }
             $result = false;
-
+            #
             if(isset($_POST["sql"])){
                 $query = $_POST["sql"];
                 if($query==null){
                     echo '<script>alert("输入为空")</script>';
                 }else{
-                    
+                    #输出所输入的查询语言
                     $sql_String = strval($_POST["sql"]);
                     $sql_String = "\"".$sql_String."\"";
                     echo "输入的sql语句为：".$sql_String;
+                    #尝试在数据库中进行sql查询
                     try{
                         $result = $link->query($query);
                     }
                     catch(Exception $e){
                         echo '<script>alert("数据操作失败，请检查输入格式")</script>';
                     }
-                    if($result){
+                    if($result != false){
                         echo "<br>以下为所有sql查询结果：";
                         echo "<br>";
-                        $result2 = mysqli_query($link,$query);
+                        $result2 = mysqli_query($link,"select *from table1");
                         while($row = mysqli_fetch_row($result2))
                         {
                             print_r($row);
-                            echo "<br>";
+                        #    echo "<br>";
                             #$message[] = $row;
                         }
                     }
+                    $query = null;
                 }
                 #$message = array($sql);
                 #while($row = mysqli_fetch_row($sql))
@@ -100,5 +105,11 @@
     }
     function passQuery(){
         getValue('sql');
+    }
+    function fflush(){
+        var input = document.querySelector('input');
+        window.addEventListener('load', function() {
+        input.value = "";
+})
     }
 </script>
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # database_sys
-This is the project for database system course
+This is the project for database system course  
+follow my steps and you will get the project  
+firstly you need to dowload some tools:  
+* Xampp
+* Cpolar
+* vscode 
+
+secondly you need to set you Xampp following this blog:  
+[博客网页](https://blog.csdn.net/baidu_25343343/article/details/53004815)  
+[bilibili 视频](https://www.bilibili.com/video/BV1hR4y1L7DT/?spm_id_from=333.337.search-card.all.click&vd_source=37cd9642f9dd0e2c4477db8a5e738a8a)
+
+Note that this is based on php for connection with mysql, you need to install xdebug for debugging the php script 
+
+Thirdly, what you need to do is write a simple html and php file to achieve the basical function that the web need to provide  
+[简单的登录界面和页面跳转](https://blog.csdn.net/qq_51861704/article/details/124702822)   
+[简单的php连接数据库](https://blog.csdn.net/qq_41027228/article/details/103979315)
+
+Note that the version of php of 7 is quite different from the old version of 5, where most of the function being called should be replaced by `mysqli` instead of `mysql` 
+
+Then, we need Cpolar in order to NAT, which is used to put you web onto the WAN, so other people can get your web service via www  
+following this [内网穿透](https://blog.csdn.net/qq_19309473/article/details/127400122)
+
+
+![演示视频一](demo.gif)  
+这里修改了部分php代码，以解决上面出现的问题  
+
+![演示视频一](demo3.gif)
